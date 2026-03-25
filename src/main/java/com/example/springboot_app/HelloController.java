975a42271b8422c31b1c41d584fe8e14f27c1623
Harshana Jayathissa <harshanajayathissa227@gmail.com>
2026-03-25 10:46:48 +0530

    test 1

--- src/main/java/com/example/springboot_app/HelloController.java
+++ src/main/java/com/example/springboot_app/HelloController.java
@@ -12,7 +12,7 @@ public class HelloController {
 
     @GetMapping("/")
     public String hello() {
-        return "Hello from Spring Boot! Environment: " + activeProfile;
+        return "Hello from Spring Boot! new Environment: " + activeProfile;
     }
 
     @GetMapping("/health-check")
