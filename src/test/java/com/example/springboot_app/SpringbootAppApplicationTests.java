6f6203265805ecac81c3466a7007c33d4d2ee791
Harshana Jayathissa <harshanajayathissa227@gmail.com>
2026-03-26 15:29:35 +0530

    Fix test package to match main class

--- src/test/java/com/example/springboot_app/SpringbootAppApplicationTests.java
+++ src/test/java/com/example/springboot_app/SpringbootAppApplicationTests.java
@@ -1,4 +1,4 @@
-package springboot_app;
+package com.example.springboot_app;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class SpringbootAppApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
